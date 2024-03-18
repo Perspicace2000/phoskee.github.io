b600d09a6d7fdc1addd01dc6ffd9ffaec6a4143b
Jacopo <125825348+phoskee@users.noreply.github.com>
2024-03-18 08:41:21 +0100

    Test changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,24 +9,10 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (SPA Mode)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/future/spa-mode"
-            rel="noreferrer"
-          >
-            SPA Mode Guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>
+        Prova
+      </h1>
     </div>
   );
 }
