0ad58eacb2038bc3e214103592cb0be0672f93a7
Jacopo <125825348+phoskee@users.noreply.github.com>
2024-03-19 11:35:56 +0100

    404

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,18 @@
 import { LinksFunction } from "@remix-run/node";
 import {
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  isRouteErrorResponse,
   useRouteError,
 } from "@remix-run/react";
 
 import stylesheet from "~/tailwind.css?url";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -16,7 +20,7 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="it-IT">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -42,16 +46,49 @@ export function HydrateFallback() {
 
 export function ErrorBoundary() {
   const error = useRouteError();
-  console.error(error);
+
   return (
-    <html>
+    <html lang="it-IT">
       <head>
-        <title>Oh no!</title>
+        <title>Oops!</title>
         <Meta />
         <Links />
       </head>
       <body>
-        Oh no!
+        <h1>
+          {isRouteErrorResponse(error) && error.status === 404 ? (
+            <div
+              className="flex flex-col place-items-center border rounded-3xl p-5"
+              style={{
+                position: "fixed",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <h1
+                className="mx-auto text-9xl animate-pulse"
+                style={{ fontFamily: "Bebas Neue, sans-serif" }}
+              >
+                404
+              </h1>
+              <h1
+                className="mx-auto animate-pulse"
+                style={{ fontFamily: "Bebas Neue, sans-serif" }}
+              >
+                (probabilmente ho dimenticato qualcosa 🥺)
+              </h1>
+              <Separator className="m-2"/>
+              <Link to={"/"}><Button variant="secondary">TORNIAMO IN UN POSTO ESISTENTE</Button></Link>
+            </div>
+          ) : isRouteErrorResponse(error) ? (
+            `${error.status} ${error.statusText}`
+          ) : error instanceof Error ? (
+            error.message
+          ) : (
+            "Errore sconosciuto"
+          )}
+        </h1>
         <Scripts />
       </body>
     </html>
