38363dc19b42490dff38b17d3f308af6cab05d25
Jacopo <125825348+phoskee@users.noreply.github.com>
2024-03-20 13:49:59 +0100

    test script

--- app/routes/test.tsx
+++ app/routes/test.tsx
@@ -6,7 +6,7 @@ import {
 import { REGEXP_ONLY_CHARS } from "input-otp";
 
 import type { MetaFunction } from "@remix-run/node";
-import React from "react";
+import React, { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -25,6 +25,149 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   const [value, setValue] = React.useState("");
+  const [paroleUnicheMap, setParoleUnicheMap] = useState([]);
+  let risultatiTrovati = [];
+
+  const avviaRicerca = async () => {
+    if (value.length < 25) {
+      alert("inserisci tutti i caratteri");
+    } else {
+      console.log("ok");
+      console.log("chiamo trovo parole");
+      // Chiamata a trovaParole dopo la sostituzione delle lettere
+      risultatiTrovati = await trovaParole(
+        value,
+        "dizionario_no_accenti_minuscolo.txt",
+        4,
+        16
+      );
+      console.log("risultati trovati", risultatiTrovati);
+
+      /*fine else*/
+    }
+    async function trovaParole(
+      letters,
+      fileParole,
+      lunghezzaMin,
+      lunghezzaMax
+    ) {
+      console.log("avvio trova parole");
+      const risultati = [];
+      const paroleDaTrovare = [];
+      // Creazione della griglia
+      const griglia = [];
+
+      for (let i = 0; i < 5; i++) {
+        griglia.push([]);
+        for (let j = 0; j < 5; j++) {
+          const id = i * 5 + j + 1; // Calcola l'ID incrementale basato sulla posizione
+          const lettera = letters[i * 5 + j];
+          griglia[i][j] = { id, lettera };
+        }
+        console.log("griglia creata");
+      }
+      console.log("griglia ", griglia);
+      console.log(
+        "test griglia[0][0].id",
+        griglia[0][0].id,
+        "test griglia[0][0].lettera",
+        griglia[0][0].lettera
+      );
+
+      await fetch(fileParole)
+        .then((response) => response.text())
+        .then((data) => {
+          console.log("fetch file parole", fileParole);
+          const parole = data.split("\n");
+          for (const parola of parole) {
+            const parolaSenzaSpazi = parola.trim().toUpperCase();
+            if (
+              parolaSenzaSpazi.length >= lunghezzaMin &&
+              parolaSenzaSpazi.length <= lunghezzaMax
+            ) {
+              paroleDaTrovare.push(parolaSenzaSpazi);
+            }
+          }
+          console.log("paroleDaTrovare lunghezza", paroleDaTrovare.length);
+        })
+        .catch((error) =>
+          console.error("Errore nel caricamento del file parole:", error)
+        );
+
+      function ricercaParole(
+        riga,
+        colonna,
+        parolaCorrente,
+        parolaRestante,
+        visitate
+      ) {
+        if (visitate.has(`${riga},${colonna}`)) {
+          return;
+        }
+
+        visitate.add(`${riga},${colonna}`);
+
+        if (parolaRestante === "") {
+          risultati.push({
+            parola: parolaCorrente,
+            coordinate: Array.from(visitate).map((coordinate) => {
+              const [row, col] = coordinate.split(",").map(Number);
+              return griglia[row][col].id;
+            }),
+          });
+          return;
+        }
+
+        const prossimaLettera = parolaRestante[0];
+
+        for (
+          let i = Math.max(0, riga - 1);
+          i < Math.min(griglia.length, riga + 2);
+          i++
+        ) {
+          for (
+            let j = Math.max(0, colonna - 1);
+            j < Math.min(griglia[0].length, colonna + 2);
+            j++
+          ) {
+            if (griglia[i][j].lettera.toUpperCase() === prossimaLettera) {
+              ricercaParole(
+                i,
+                j,
+                parolaCorrente + griglia[i][j].lettera,
+                parolaRestante.slice(1),
+                new Set(visitate)
+              );
+            }
+          }
+        }
+      }
+
+      const paroleUniche = Array.from(new Set(paroleDaTrovare)); // Rimuovi duplicati
+      paroleUniche.sort((a, b) => b.length - a.length); // Ordina dalla più lunga alla più corta
+
+      for (const parola of paroleUniche) {
+        for (let i = 0; i < griglia.length; i++) {
+          for (let j = 0; j < griglia[0].length; j++) {
+            if (griglia[i][j].lettera === parola[0]) {
+              ricercaParole(
+                i,
+                j,
+                griglia[i][j].lettera,
+                parola.slice(1),
+                new Set()
+              );
+            }
+          }
+        }
+      }
+      console.log("paroleUniche");
+      console.log(paroleUniche);
+      
+      setParoleUnicheMap(paroleUniche)
+      return risultati;
+    }
+  };
 
   return (
     <div className="flex place-items-center border border-red-500 min-h-svh ">
@@ -45,74 +188,24 @@ export default function Index() {
           </InputOTP>
         </div>
         <div className="grid rounded-xl bg-pink-200 w-full p-2 gap-2">
-        <Button id="crea-griglia-btn" className="rounded-xl" variant={"secondary"}>
-           Avvia ricerca parole
+          <Button
+            id="ricerca-btn"
+            className="rounded-xl"
+            variant={"secondary"}
+            onClick={avviaRicerca}
+          >
+            Avvia ricerca parole
           </Button>
           <Select>
             <SelectTrigger className="w-full rounded-xl bg-white p-2">
-              <SelectValue placeholder="Select a timezone" />
+              <SelectValue placeholder="Seleziona una parola" />
             </SelectTrigger>
             <SelectContent>
-              <SelectGroup>
-                <SelectLabel>North America</SelectLabel>
-                <SelectItem value="est">Eastern Standard Time (EST)</SelectItem>
-                <SelectItem value="cst">Central Standard Time (CST)</SelectItem>
-                <SelectItem value="mst">
-                  Mountain Standard Time (MST)
+              {paroleUnicheMap.map((parola, index) => (
+                <SelectItem key={index} value={parola}>
+                  {parola}
                 </SelectItem>
-                <SelectItem value="pst">Pacific Standard Time (PST)</SelectItem>
-                <SelectItem value="akst">
-                  Alaska Standard Time (AKST)
-                </SelectItem>
-                <SelectItem value="hst">Hawaii Standard Time (HST)</SelectItem>
-              </SelectGroup>
-              <SelectGroup>
-                <SelectLabel>Europe & Africa</SelectLabel>
-                <SelectItem value="gmt">Greenwich Mean Time (GMT)</SelectItem>
-                <SelectItem value="cet">Central European Time (CET)</SelectItem>
-                <SelectItem value="eet">Eastern European Time (EET)</SelectItem>
-                <SelectItem value="west">
-                  Western European Summer Time (WEST)
-                </SelectItem>
-                <SelectItem value="cat">Central Africa Time (CAT)</SelectItem>
-                <SelectItem value="eat">East Africa Time (EAT)</SelectItem>
-              </SelectGroup>
-              <SelectGroup>
-                <SelectLabel>Asia</SelectLabel>
-                <SelectItem value="msk">Moscow Time (MSK)</SelectItem>
-                <SelectItem value="ist">India Standard Time (IST)</SelectItem>
-                <SelectItem value="cst_china">
-                  China Standard Time (CST)
-                </SelectItem>
-                <SelectItem value="jst">Japan Standard Time (JST)</SelectItem>
-                <SelectItem value="kst">Korea Standard Time (KST)</SelectItem>
-                <SelectItem value="ist_indonesia">
-                  Indonesia Central Standard Time (WITA)
-                </SelectItem>
-              </SelectGroup>
-              <SelectGroup>
-                <SelectLabel>Australia & Pacific</SelectLabel>
-                <SelectItem value="awst">
-                  Australian Western Standard Time (AWST)
-                </SelectItem>
-                <SelectItem value="acst">
-                  Australian Central Standard Time (ACST)
-                </SelectItem>
-                <SelectItem value="aest">
-                  Australian Eastern Standard Time (AEST)
-                </SelectItem>
-                <SelectItem value="nzst">
-                  New Zealand Standard Time (NZST)
-                </SelectItem>
-                <SelectItem value="fjt">Fiji Time (FJT)</SelectItem>
-              </SelectGroup>
-              <SelectGroup>
-                <SelectLabel>South America</SelectLabel>
-                <SelectItem value="art">Argentina Time (ART)</SelectItem>
-                <SelectItem value="bot">Bolivia Time (BOT)</SelectItem>
-                <SelectItem value="brt">Brasilia Time (BRT)</SelectItem>
-                <SelectItem value="clt">Chile Standard Time (CLT)</SelectItem>
-              </SelectGroup>
+              ))}
             </SelectContent>
           </Select>
           <Button id="reset-btn" className="rounded-xl" variant={"secondary"}>
